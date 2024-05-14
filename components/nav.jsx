87ce16d84f465a7e26f3b76aeee2abb2280ae4a5
syedubaid <syedabuubaid@gmail.com>
2024-05-14 22:40:27 +0530

    commit

--- components/nav.jsx
+++ components/nav.jsx
@@ -3,7 +3,7 @@ export default function Navbar(){
         <>
         
             <div className="nav-content">
-                <img className="image" src="./public/static/images/trollface.png"></img>
+                <img className="image" src="./static/images/trollface.png"></img>
                 <h3 className="heading">Meme Generator</h3>
                 <h3 className="github"><a href="https://github.com/syedubaid11/meme-generator">Github</a></h3>
             </div>
